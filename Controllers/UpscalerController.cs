c3a5f31b2aae31bcbc45a0a5fe20af0f09a4c419
Kuschel-code <kuschel-code@users.noreply.github.com>
2026-01-14 09:57:50 +0100

    "v1.4.7.1_EnableInMainMenu_fix"

--- Controllers/UpscalerController.cs
+++ Controllers/UpscalerController.cs
@@ -211,7 +211,7 @@ namespace JellyfinUpscalerPlugin.Controllers
                     FFmpegAvailable = true,
                     OnnxRuntime = "Available",
                     Platform = Environment.OSVersion.Platform.ToString(),
-                    PluginVersion = "1.4.7"
+                    PluginVersion = "1.4.7.1"
                 });
             }
             catch (Exception ex)
